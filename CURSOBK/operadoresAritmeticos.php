05a4b2f194fd06ca7a64b7b12e6569a577096e32
Vero Campero <130777997+verocampero@users.noreply.github.com>
2024-02-14 16:47:43 -0300

    Add files via upload

---
+++ CURSOBK/operadoresAritmeticos.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+$division =12/2;
+$multiplicacion = 2*3;
+$modulo = 20%3; //20/3=6, resto es lo que va a mostrar que es 2
+$resta = 15-10;
+$suma = 15+10;
+
+// la variable $numero se muestra primero con su numero asignado luego incrementa un numero
+$numero = 25;
+echo $numero++;
+echo $numero;
+
+
+
+
+
+// echo $modulo+$resta + $suma;
+
+
+
+
+
+
+
+?>
---
+++ CURSOBK/operadoresDeAsignación.php
@@ -0,0 +1,42 @@
+<?php
+
+$a = ($b=5)+10;
+
+
+//operadores de asignacion mas igual 
+$c = 3;
+$c +=5;
+
+
+
+//Operadores de asignacion menos igual
+
+$num1 = 15;
+$num1 -= 10;
+
+
+
+//Operadores de asignacion divido igual
+
+
+$num2 = 15;
+$num2 /= 10;
+
+
+echo $num2;
+
+//Operador multiplicacion igual
+
+$num3 = 15;
+$num3 *= 10;
+
+// Operadores de concatenado .= 
+
+$saludo = 'Hola como estas ? ';
+$nombre = 'Vero';
+$saludo.= $nombre;
+
+
+
+
+?>
