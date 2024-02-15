a8fe25ca501c55d3b37d332c438991158e63093c
verocampero <verocampero2@gmail.com>
2024-02-15 00:31:59 -0300

    agregue ejercicios

---
+++ CURSOBK/ejercicios.php
@@ -0,0 +1,19 @@
+<?php
+
+
+// realiza un programa que tome 2 numeros y muestre la suma,  resta multiplicacion y division de esos 2 numeros
+
+$num1 = 15;
+$num2= 2;
+
+echo $num1 + $num2, '<br>';
+echo $num1 - $num2, '<br>';
+echo $num1 * $num2, '<br>';
+echo $num1 / $num2, '<br>';
+
+
+
+
+
+
+?>
